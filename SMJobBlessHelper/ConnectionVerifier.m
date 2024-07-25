f4a29a0d2f58c208eefb59f518defbc9e89f583a
Void <marlkiller@voidm.com>
2024-07-25 14:10:58 +0800

    dev

--- SMJobBlessHelper/ConnectionVerifier.m
+++ SMJobBlessHelper/ConnectionVerifier.m
@@ -20,6 +20,7 @@
 @implementation ConnectionVerifier
 
 // ref : https://github.com/PhorionTech/Kronos/blob/0d176e19d702e01d6bcc1f1ff7681b946dc9ee6f/TCCKronosExtension/XPC/ConnectionVerifier.m
+// ref : https://developer.apple.com/documentation/security/1395809-seccodecopysigninginformation
 + (BOOL)isValid:(NSXPCConnection*)connection {
     
     
@@ -27,7 +28,7 @@
     OSStatus status = !errSecSuccess;
     CFErrorRef error = nil;
 
-    //extract audit token
+    // 审计令牌 这个结构体通常在进程间通信或系统调用时由内核提供。
     audit_token_t auditToken = {0};
     auditToken = connection.auditToken;
     SecTaskRef taskRef = 0;
@@ -42,7 +43,7 @@
     NSString* requirement = nil;
     
     
-    //obtain dynamic code ref
+    // 用于根据指定的属性创建或获取一个代码对象,这里根据主应用程序的审计令牌获取到的主应用程序的 SecCodeRef
     status = SecCodeCopyGuestWithAttributes(NULL, (__bridge CFDictionaryRef)(@{
         (NSString *)kSecGuestAttributeAudit : [NSData dataWithBytes:&auditToken length:sizeof(audit_token_t)]
     }), kSecCSDefaultFlags, &codeRef);
@@ -53,7 +54,9 @@
         return NO;
     }
     
-    // 获取 SecStaticCodeRef
+    // 从一个动态代码对象 (SecCodeRef) 创建一个静态代码对象 (SecStaticCodeRef)
+    // SecCodeRef : 表示正在运行的进程或已加载的可执行文件的代码签名,包含动态属性，如进程的当前状态、安全性上下文等。
+    // SecStaticCodeRef : 表示一个不可变的、静态的代码签名。通常用于代码签名验证、签名信息提取等静态操作。
     SecStaticCodeRef staticCodeRef = NULL;
     status = SecCodeCopyStaticCode(codeRef, kSecCSDefaultFlags, &staticCodeRef);
     if (status != errSecSuccess) {
@@ -61,7 +64,7 @@
         return NO;
     }
 
-    // 使用 SecStaticCodeCheckValidity 进行校验
+    // 检查静态代码对象 (SecStaticCodeRef) 的签名有效性
     status = SecStaticCodeCheckValidity(staticCodeRef, 0xd, 0);
     if (status != errSecSuccess) {
         NSLog(@">>>>>> SecStaticCodeCheckValidity failed with status = %d", (int)status);
@@ -78,7 +81,7 @@
     // }
     
     // TODO 这里应该是 staticCodeRef
-    //get code signing info
+    // 获取代码对象的签名信息。通过调用这个函数，你可以提取代码签名的详细信息，例如签名证书、签名时间等
     status = SecCodeCopySigningInformation(codeRef, kSecCSSigningInformation, &csInfo);
     if(errSecSuccess != status)
     {
@@ -88,7 +91,7 @@
     
     
     // TODO flags
-    // flags
+    // 检查签名是否有效 flags ?? 
     SecCSFlags flags;
     CFNumberRef flagsNumber = (CFNumberRef)CFDictionaryGetValue(csInfo, kSecCodeInfoFlags);
    if (flagsNumber == NULL) {
@@ -132,7 +135,7 @@
 
     
     
-    // 使用 SecRequirementCreateWithString 进行校验
+    // 验证代码的签名是否符合指定的条件
     // requirement = @"anchor apple generic and certificate leaf[subject.CN] = \"Apple Development: marlkiller@vip.qq.com (L79ZQ6T579)\"";
     requirement = [NSString stringWithFormat:@"identifier \"com.apple.bsd.SMJobBlessApp\" and anchor apple generic and certificate leaf[subject.CN] = \"Apple Development: marlkiller@vip.qq.com (L79ZQ6T579)\" and certificate 1[field.1.2.840.113635.100.6.2.1] /* exists */"];
     SecRequirementRef requirement_ref = NULL;
@@ -143,6 +146,7 @@
     }
     NSLog(@">>>>>> requirement_ref = %@",requirement_ref);
     
+    // 验证代码的签名是否符合特定的要求，并返回详细的错误信息
     status = SecCodeCheckValidityWithErrors(codeRef, kSecCSDefaultFlags, requirement_ref,&error);
     if (status != errSecSuccess) {
         NSLog(@">>>>>> SecCodeCheckValidityWithErrors failed with status = %d", (int)status);
